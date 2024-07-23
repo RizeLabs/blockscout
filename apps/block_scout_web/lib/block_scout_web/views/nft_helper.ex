a90e55e5e0ef4404d886212709a0819745f60189
Nikita Pozdniakov <nikitosing4@mail.ru>
2024-07-23 18:49:11 +0300

    Remain to test

--- apps/block_scout_web/lib/block_scout_web/views/nft_helper.ex
+++ apps/block_scout_web/lib/block_scout_web/views/nft_helper.ex
@@ -18,6 +18,9 @@ defmodule BlockScoutWeb.NFTHelper do
         metadata["image"] ->
           retrieve_image(metadata["image"])
 
+        is_binary(metadata["properties"]["image"]) ->
+          metadata["properties"]["image"]
+
         metadata["properties"]["image"]["description"] ->
           metadata["properties"]["image"]["description"]
 
